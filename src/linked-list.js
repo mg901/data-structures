70c197009da7978878c6192d7ee7a4cb949c79de
Maxim Alyoshin <mgreen097@gmail.com>
2022-12-12 18:52:19 +0300

    feat: 🎸 add prepend method

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,19 +1,52 @@
+const { LinkedListNode } = require('./linked-list-node');
+
 exports.LinkedList = class LinkedList {
-  constructor() {
+  /**
+   *
+   * @param {Function} [comparatorFunction]
+   */
+  constructor(comparatorFunction) {
+    /** @var LinkedListNode */
     this.head = null;
+
+    /** @var LinkedListNode */
     this.tail = null;
+    this.compare = comparatorFunction;
   }
 
+  /**
+   * @param {*} value
+   * @return {LinkedList}
+   */
+  prepend(value) {
+    // Make new node to be head
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+
+    // If there is no tail yet let's make new node a tail
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {*} value
+   * @return {LinkedList}
+   */
   append(value) {
     const newNode = new LinkedListNode(value);
 
-    if (!this.head || !this.tail) {
+    // If there is no head yet let's make new node a head
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
 
       return this;
     }
 
+    // Attach new node to the end of linked list
     this.tail.next = newNode;
     this.tail = newNode;
 
